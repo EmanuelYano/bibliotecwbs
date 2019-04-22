ace487bb45be0e3050b69eb3020f9850c706d3f8
JeanRM <jean.martins0000@gmail.com>
2019-04-22 15:17:56 -0400

    cadastro

--- teste/src/views/Cadastro.vue
+++ teste/src/views/Cadastro.vue
@@ -1,42 +1,62 @@
 <template>
     <div>
-
+      <v-container>
         <v-form>
-            <v-container>
-              <v-layout align-end justify-center column >
-        
-                <v-flex xs12 sm6 md7>
+           <v-layout align-center justify-center column class="elevation-2">
+             <v-flex xs12 sm6 md12>
                   <v-text-field
                     label="Nome"
                   ></v-text-field>
                 </v-flex>
         
-                <v-flex xs12 sm6 md7>
+                <v-flex xs12 sm6 md12>
                   <v-text-field
-                    label="Codigo do Aluno"
+                    label="Código do Aluno"
                   ></v-text-field>
                 </v-flex>
         
-                <v-flex xs12 sm6 md7>
+                <v-flex xs12 sm6 md12>
                     <v-text-field
-                      label="Email"
+                      label="E-mail"
                     ></v-text-field>
                   </v-flex>
 
-                  <v-flex xs12 sm6 md7>
+                  <v-flex xs12 sm6 md12>
                     <v-text-field
-                      label="Numero de Telefone"
+                      label="Número de Telefone"
                     ></v-text-field>
                   </v-flex>
                   
-                  <v-flex xs12 sm6 md7>
+                  <v-flex xs12 sm6 md12>
                     <v-text-field
-                      label="Serie"
+                      label="Série"
                     ></v-text-field>
                   </v-flex>
-            
+
+                  <v-flex xs12 sm6 md12>
+                      <v-text-field
+                      v-model="password"
+                      :append-icon="show1 ? 'visibility' : 'visibility_off'"
+                      :rules="[rules.required, rules.min]"
+                      :type="show1 ? 'text' : 'password'"
+                      name="input-10-1"
+                      label="Normal with hint text"
+                      hint="At least 8 characters"
+                      counter
+                      @click:append="show1 = !show1"
+                      ></v-text-field>
+                    </v-flex>
+
               </v-layout>
-            </v-container>
-          </v-form>
+            </v-form>
+      </v-container>
+
+      <v-layout align-center justify-center>
+          <div>
+              
+                <v-btn color="green">confirmar</v-btn>
+            
+          </div>
+      </v-layout>    
     </div>
-</template>
+</template>'''''''''''''  '
